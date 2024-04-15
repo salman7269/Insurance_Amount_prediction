c8b662a7d92a23362c79dae64b8d80459044b6d2
salman7269 <salmanvelocity5269@gmail.com>
2024-04-15 20:47:55 +0530

    interface

--- interface.py
+++ interface.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import config
 from Project.utils import Medical_Insurance
 import numpy as np
@@ -6,7 +6,12 @@ import numpy as np
 app=Flask(__name__)
 @app.route("/")
 def get_home():
-    return "Hello Welcome to HomePage."
+    return "Hello Welcome to the ML project. Let's predict the future."
+
+@app.route("/html_page")
+def html():
+    return render_template("index.html")
+
 
 @app.route("/predict_charges",methods= ["POST","GET"])
 def get_insurance():
@@ -23,6 +28,8 @@ def get_insurance():
         med_obj=Medical_Insurance(age,gender,bmi,children,smoker,region)
         charges=med_obj.get_predicted_charges()
         return  jsonify({"Result":f"Predicted Medical Charges are: {charges[0]}"})
+    
+        
     
 if __name__=="__main__":
     app.run()
